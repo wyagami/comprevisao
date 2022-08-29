91e036e269424caef95b70526b2c392e3a9c13a9
wyagami <43221038+wyagami@users.noreply.github.com>
2022-08-29 14:59:27 -0300

    Add files via upload

--- Comprevisao.py
+++ Comprevisao.py
@@ -142,7 +142,7 @@ try:
         w22 = words['Preco'][v]
         v = v+1
         list_cutoff = [1,0.9,0.8,0.7,0.6,0.5]
-        for cutoff in list_cutoff:
+        for cutoff in list_cutoff and not w1 in removidos:
           similarity_sentence_text_bigram = get_sentence_similarity(w1, w2, use_text_bigram=True)
           if similarity_sentence_text_bigram >= similarity_sentence_text_bigram_ant and similarity_sentence_text_bigram  > 0 and similarity_sentence_text_bigram  >= cutoff:
             insert(df,['{:<100}'.format(str(w2)),'{:>10}'.format(str(w22)),'{:<100}'.format(str(w1)),'{:>15}'.format(str(w11)),'{:>15}'.format(str(float(w11)*float(w22)))])
